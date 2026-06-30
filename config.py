3acddc3fc5e9445f9330b34df5c6cbf44048b2f6
mr185888888-afk <mr185888888@gmail.com>
2026-06-30 06:34:30 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    BOT_TOKEN = os.getenv('BOT_TOKEN')
+    
+    @classmethod
+    def validate(cls):
+        if not cls.BOT_TOKEN:
+            raise ValueError("BOT_TOKEN not found in .env file")
+        return True
+
+Config.validate()
